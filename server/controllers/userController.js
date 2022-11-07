7514403e49eb5fe3982fb42426d0ef90ba8d2af4
Indranil Halder <indranilhalder.dev@gmail.com>
2022-11-08 01:54:44 +0530

    Register and Login Functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,7 @@
 import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/userModel";
 
 // @desc    Register a new user
 // @route   /api/users
@@ -10,13 +13,59 @@ const register = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please fill up all the fields!");
   }
+  // Find if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists!");
+  }
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User can not be created!");
+  }
 });
 
 // @desc    Login user
 // @route   /api/users/login
 // @access  Public
 const login = asyncHandler(async (req, res) => {
-  res.send("Login");
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  // Check user and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password!");
+  }
 });
+
+// Generate Token
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
 
 export default { register, login };
---
+++ server/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email!"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add password!"],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
