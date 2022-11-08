7d7491b362473427ab774339f1c59f6cf722bde6
Indranil Halder <indranilhalder.dev@gmail.com>
2022-11-08 23:51:43 +0530

    Set up the client

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "react-bootstrap/Button";
+
+const App = () => {
+  return (
+    <>
+      <h1>App Component</h1>
+      <Button variant="primary">Click Me</Button>
+    </>
+  );
+};
+
+export default App;
